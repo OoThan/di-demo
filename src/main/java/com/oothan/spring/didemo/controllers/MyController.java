ad6ad80f59a512e86fcc2b6c23026b86b731eff0
OoThan <mgoothan.psnl@gmail.com>
2022-06-03 18:44:50 +0630

    Primary Annotation

--- src/main/java/com/oothan/spring/didemo/controllers/MyController.java
+++ src/main/java/com/oothan/spring/didemo/controllers/MyController.java
@@ -1,13 +1,20 @@
 package com.oothan.spring.didemo.controllers;
 
+import com.oothan.spring.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("Hello !!");
-        return "hello";
+        return greetingService.sayGreeting();
     }
 
 }
--- src/main/java/com/oothan/spring/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/oothan/spring/didemo/controllers/PropertyInjectedController.java
@@ -2,12 +2,14 @@ package com.oothan.spring.didemo.controllers;
 
 import com.oothan.spring.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingServiceImpl greetingServiceImpl;
 
     public String sayHello() {
